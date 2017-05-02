1b97c0c2a50a7e353d36f7e7517b3fb61b81c4ea
Anthony <anthonyguitarman13@gmail.com>
2017-05-01 18:17:42 -0600

    App compnent created

--- musicmaster/src/index.js
+++ musicmaster/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
 ReactDom.render(
-    <div>Music Master</div>, document.getElementById('root')
+   <App />, document.getElementById('root')
 );
